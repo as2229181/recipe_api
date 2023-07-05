7dd2053604e3c12dd0f9b6aa04bd97d546f1489f
as2229181 <as2229181@gmail.com>
2023-07-05 22:37:34 +0800

    add wait_for_db v5

--- app/app/test.py
+++ app/app/test.py
@@ -5,16 +5,18 @@ from django.test import SimpleTestCase
 
 from . import cal
 
+
 class CalcTest(SimpleTestCase):
-        """Test cal module"""
-        def test_add_number(self):
-            x = 1
-            y = 1
-            res = cal.add(x,y)
-            self.assertEqual(res,2)
+    """Test cal module"""
 
-        def test_substract_numbers(self):
-            """Test subtract numbers."""
-            res = cal.subtract(10, 15)
+    def test_add_number(self):
+        x = 1
+        y = 1
+        res = cal.add(x, y)
+        self.assertEqual(res, 2)
 
-            self.assertEqual(res, 5)        
+    def test_substract_numbers(self):
+        """Test subtract numbers."""
+        res = cal.subtract(10, 15)
+
+        self.assertEqual(res, 5)
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -13,22 +13,23 @@ from django.test import SimpleTestCase
 @patch("core.management.commands.wait_for_db.Command.check")
 class CommandTests(SimpleTestCase):
     """Test command."""
-     
+
     def test_wait_for_db_ready(self, patch_check):
         """Test waiting for database if database ready."""
         patch_check.return_value = True
-        
-        call_command('wait_for_db')
 
-        patch_check.assert_called_once_with(databases=['default'])
-    
-    @patch('time.sleep')
+        call_command("wait_for_db")
+
+        patch_check.assert_called_once_with(databases=["default"])
+
+    @patch("time.sleep")
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError."""
-        patched_check.side_effect = [Psycopg2Error] * 2 +\
-            [OperationalError] * 3 + [True]
+        patched_check.side_effect = (
+            [Psycopg2Error] * 2 + [OperationalError] * 3 + [True]
+        )
 
-        call_command('wait_for_db')
-        
+        call_command("wait_for_db")
+
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(databases=['default'])
+        patched_check.assert_called_with(databases=["default"])
